e8ce04afd049ca4de66b6bf862defdc9ae602da9
Giacomo Righetti <kirkrulez83@gmail.com>
2020-02-24 14:58:53 +0000

    Added example of async action

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,14 +1,21 @@
 import { ADD_ARTICLE } from "../constants/action-types";
+import { DATA_LOADED } from "../constants/action-types";
 
 const initialState = {
-  articles: []
+  articles: [],
+  remoteArticles: []
 };
 
 function rootReducer(state = initialState, action) {
   if (action.type === ADD_ARTICLE) {
-    // create a new copy of the state by merging the previous state with the new articles collection
     return Object.assign({}, state, {
       articles: state.articles.concat(action.payload)
+    });
+  }
+
+  if (action.type === DATA_LOADED) {
+    return Object.assign({}, state, {
+      remoteArticles: state.remoteArticles.concat(action.payload)
     });
   }
   return state;
